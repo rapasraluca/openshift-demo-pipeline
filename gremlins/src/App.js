188379badf22f9f8a3b38ea6d61c1f543fa25d65
rapasraluca <53560380+rapasraluca@users.noreply.github.com>
2021-10-04 17:40:04 +0300

    Update App.js

--- gremlins/src/App.js
+++ gremlins/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gremlins need to learn <code>React</code> fast !.
+          Gremlins need to learn <code>React</code> fast !. test
         </p>
         <a
           className="App-link"
